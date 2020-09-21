b824a5a810d3b78e9abb06c4fe0ed8f6b6f5058a
Elitrirudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-09-21 08:08:42 +0700

    Add files via upload

---
+++ tugas9.js
@@ -0,0 +1,13 @@
+function panggilObject(){
+var Pegawai={
+	nama:"Ratih",
+	umur:25,
+	Bagian:"Akunting",
+	Jabatan:"Staff"
+}
+for (var x in Pegawai){
+	console.log(Pegawai [x])
+}
+
+}
+panggilObject()
